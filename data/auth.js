4a95ccc1993729677ba5a8400b31eac40b6a4300
Sheen (Shen) Tian <limshion@gmail.com>
2017-09-03 17:01:53 +0800

    add morgan logger

--- data/auth.js
+++ data/auth.js
@@ -1,8 +1,8 @@
 'use strict';
-var HttpError   = require('http-errors');
-var Config      = require('../config/config.js');
-var AuthJwt     = require('../security/auth-jwt.js');
-var db          = require('../models');
+var HttpError = require('http-errors');
+var Config = require('../config/config.js');
+var AuthJwt = require('../security/auth-jwt.js');
+var db = require('../models');
 
 /**
  * Operations on /auth
@@ -24,7 +24,7 @@ module.exports = {
             }
 
             // Communicate with WX server to get the open id and session key
-            
+
             var wxOpenId = wxLoginCode;
             var wxSessionKey = '4b-6kQQrOFC1UyBiZqHnMZd/6C4oMg';
 
@@ -34,27 +34,27 @@ module.exports = {
                     wxOpenId: wxOpenId,
                 }
             })
-            .then(result => {
-                const [user, created] = result;
-                // If this is a new crated user then create cardcase for it
-                if (created) {
-                    db.Cardcase.create({
-                        userId: user.id
-                    });
-                }
-                return user;
-            })
-            .then(user => {
-                var userInfo = {
-                    id: user.id,
-                    wxOpenId: user.wxOpenId,
-                };
-                userInfo.token = AuthJwt.issueToken(userInfo, Config.tokenSecret);
-                callback(null, { responses: userInfo });
-            })
-            .catch(err => {
-                callback(new HttpError.InternalServerError(err));
-            });
+                .then(result => {
+                    const [user, created] = result;
+                    // If this is a new crated user then create cardcase for it
+                    if (created) {
+                        db.Cardcase.create({
+                            userId: user.id
+                        });
+                    }
+                    return user;
+                })
+                .then(user => {
+                    var userInfo = {
+                        id: user.id,
+                        wxOpenId: user.wxOpenId,
+                    };
+                    userInfo.token = AuthJwt.issueToken(userInfo, Config.tokenSecret);
+                    callback(null, { responses: userInfo });
+                })
+                .catch(err => {
+                    callback(new HttpError.InternalServerError(err));
+                });
         }
     }
 };
--- server.js
+++ server.js
@@ -1,44 +1,42 @@
 'use strict';
 
-var Http        = require('http');
-var Express     = require('express');
-var BodyParser  = require('body-parser');
-var Swaggerize  = require('swaggerize-express');
-var SwaggerUi   = require('swaggerize-ui');
-var Path        = require('path');
-var Config      = require('./config/config.js');
-var db          = require('./models');
+var Http = require('http');
+var Express = require('express');
+var Logger = require('morgan');
+var BodyParser = require('body-parser');
+var Swaggerize = require('swaggerize-express');
+var SwaggerUi = require('swaggerize-ui');
+var Path = require('path');
+var Config = require('./config/config.js');
+var db = require('./models');
 
 db.sequelize.sync();
 
 // Create Express instance
-var App = Express();
-
-// Create Server instance
-var Server = Http.createServer(App);
-
-// Add body parser middleware
-App.use(BodyParser.json());
-App.use(BodyParser.urlencoded({ extended: true }));
-
-// Add swagger UI middleware
-App.use('/viewer', SwaggerUi({ docs: '/api/docs' }));
-
-// Add swaggerize middleware
-App.use(Swaggerize({
-    api: Path.resolve('./config/swagger.yml'),
-    handlers: Path.resolve('./handlers'),
-    security: Path.resolve('./security'),
-    docspath: '/docs'
-}));
+var App = Express()
+    .use(Logger('dev'))         // Add logger
+    .use(BodyParser.json())     // Add json body parser
+    .use(BodyParser.urlencoded({
+        extended: true
+    }))                         // Add URL encode parser
+    .use(Swaggerize({
+        api: Path.resolve('./config/swagger.yml'),
+        handlers: Path.resolve('./handlers'),
+        security: Path.resolve('./security'),
+        docspath: '/docs'
+    }))                         // Add swagger API
+    .use('/viewer', SwaggerUi({
+        docs: '/api/docs'
+    }));                        // Add swagger ui
 
 // Start the server
-Server.listen(Config.port, function () {
-    App.swagger.api.host = Config.host;
-    if (this.address().port != undefined) {
-        App.swagger.api.host += (':' + this.address().port);
-    }
-    /* eslint-disable no-console */
-    console.log('App running on %s', App.swagger.api.host);
-    /* eslint-disable no-console */
-});
+Http.createServer(App)
+    .listen(Config.port, function () {
+        App.swagger.api.host = Config.host;
+        if (this.address().port != undefined) {
+            App.swagger.api.host += (':' + this.address().port);
+        }
+        /* eslint-disable no-console */
+        console.log('App running on %s', App.swagger.api.host);
+        /* eslint-disable no-console */
+    });
