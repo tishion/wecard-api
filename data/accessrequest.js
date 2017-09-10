7ac082d3f8b89222418183052fbd37d6085f0abf
Sheen (Shen) Tian <limshion@gmail.com>
2017-09-10 12:59:30 +0800

    access request

--- data/accessrequest.js
+++ data/accessrequest.js
@@ -1,6 +1,7 @@
 'use strict';
 var HttpError = require('http-errors');
 var Validator = require('./validator/validator.js');
+var ErrorCode = require('../error/code.json');
 var db = require('../models');
 var Mockgen = require('./mockgen.js');
 /**
@@ -51,15 +52,35 @@ module.exports = {
      */
     post: {
         200: function (req, res, callback) {
-            /**
-             * Using mock data generator module.
-             * Replace this by actual data for the api.
-             */
-            Mockgen().responses({
-                path: '/accessrequest',
-                operation: 'post',
-                response: '200'
-            }, callback);
+            var accessRequest = req.body;
+
+            return db.Namecard.findById(accessRequest.namecardId)
+            .then(namecard => {
+                if (namecard) {
+                    return db.AccessRequest.findOrCreate({
+                        where: {
+                            namecardId: accessRequest.namecardId,
+                            fromUserId: accessRequest.fromUserId,
+                            toUserId: namecard.userId
+                        }
+                    });
+                } else {
+                    throw new HttpError.BadRequest(ErrorCode.err_namecardNotFound);
+                }
+            }).spread((request, created) => {
+                if (created) {
+                    return callback({
+                        responses: request
+                    });
+                } else 
+                {
+                    throw new HttpError.Conflict('Already exist');
+                }
+            }).catch(db.sequelize.Error, err => {
+                return callback(new HttpError.InternalServerError(err));
+            }).catch(err => {
+                return callback(err);
+            });
         }
     }
 };
