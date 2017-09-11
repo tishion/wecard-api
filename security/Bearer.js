ddfb0335e06b5fea87bdf162e7865fff06154078
Sheen (Shen) Tian <limshion@gmail.com>
2017-09-11 23:53:26 +0800

    remove logs

--- security/Bearer.js
+++ security/Bearer.js
@@ -13,14 +13,14 @@ module.exports = function authorize(req, res, next) {
     //this.in - The location of the API key ("query" or "header") for securityDefinitions:Bearer apiKey security scheme.
     // var authorization = this.in.get(this.name);
     var authorization = req.get('Authorization');
-    console.log('Authorization: %s', authorization);
+    //console.log('Authorization: %s', authorization);
     if (typeof authorization !== 'undefined' && authorization) {
         var sa = authorization.split(' ');
         if (2 == sa.length && sa[0] == 'Bearer') {
 
             var user = AuthJwt.verifyToken(sa[1], Config.tokenSecret);
             if (user && user.id) {
-                console.log('User Id: %s', user.id);
+                //console.log('User Id: %s', user.id);
                 req.session = { userId: user.id };
                 return next();
             }
