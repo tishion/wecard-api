6d606231a890908b923824c414287768588697d6
Sheen (Shen) Tian <limshion@gmail.com>
2017-09-22 00:46:58 +0800

    add region transform

--- config/config.js
+++ config/config.js
@@ -3,6 +3,15 @@
 var ConfigJson  = require('./config.json');
 
 var env = process.env.NODE_ENV || 'development';
-var envConfig = ConfigJson[env];
-envConfig.port = envConfig.port || process.env.PORT;
+var config = ConfigJson[env];
+
+var region = process.env.AZURE_REGION;
+var regionConfig = config[region];
+
+for (var attr in regionConfig) {
+    config[attr] = regionConfig[attr];
+}
+
+config.port = config.port || process.env.PORT;
+
 module.exports = envConfig;
