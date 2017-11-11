58da55f9b48563a631ff66da89afdde61b757291
Sheen (Shen) Tian <limshion@gmail.com>
2017-11-11 18:04:37 +0800

    verify the membership before get group info

--- data/group/{id}.js
+++ data/group/{id}.js
@@ -17,18 +17,32 @@ module.exports = {
     get: {
         200: function (req, res, callback) {
             return db.Group.findById(req.params.id)
-            .then(group => {
-                if (!group) {
-                    throw new HttpError.NotFound();
-                }
-                return callback(null, {
-                    responses: group.prune
+                .then(group => {
+                    if (!group) {
+                        throw new HttpError.NotFound();
+                    }
+                    return Promise.all([
+                        group,
+                        db.GroupMember.findOne({
+                            where: {
+                                groupId: group.id,
+                                userId: req.session.userId
+                            }
+                        })
+                    ]);
+                }).spread((group, self) => {
+                    // If the current user doesn't exist in the group then reject the request
+                    if (!self) {
+                        throw new HttpError.Forbidden(ErrorCode.err_alienUserForbidden);
+                    }
+                    return callback(null, {
+                        responses: group.prune
+                    });
+                }).catch(db.sequelize.Error, err => {
+                    return callback(new HttpError.InternalServerError(err));
+                }).catch(err => {
+                    return callback(err);
                 });
-            }).catch(db.sequelize.Error, err => {
-                return callback(new HttpError.InternalServerError(err));
-            }).catch(err => {
-                return callback(err);
-            });
         }
     }
 };
